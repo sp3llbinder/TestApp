1764b5c6a27e3c4239127d50edb214122a036215
sp3llbinder <yves.lang@gmail.com>
2012-10-31 23:37:06 +0100

    quickfix

--- src/ch/kenshiro/testapp/MainActivity.java
+++ src/ch/kenshiro/testapp/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
